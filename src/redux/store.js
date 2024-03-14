a889ae6c57f32d935c98eefe88ad0ca76d7b0138
lsh <923717838@qq.com>
2024-03-14 21:14:22 +0800

    redux-logger中间件的使用

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
 import addNameReducer from "./addNameReducer";
 import CounterReducer from "./CounterReducer";
-
+import reduxLogger from 'redux-logger'
 // 原生redux 合并reducer 派发不影响，只影响取值
 // store.getState()  变为  store.getState().CountingReducer
 const rootReducer = combineReducers({
@@ -10,5 +10,6 @@ const rootReducer = combineReducers({
 })
 
 // createStore第二个参数代表默认状态， 一般在reducer处理
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+    applyMiddleware(reduxLogger))
 export default store
